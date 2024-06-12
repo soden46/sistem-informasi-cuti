d8f24c3533819ea60a09b360e3c1dc9bb91bbeec
Soden46 <syarifsoden22@gmail.com>
2024-06-12 22:41:50 +0700

    Update

--- resources/views/layouts/beckend/dashboard/sidebar.blade.php
+++ resources/views/layouts/beckend/dashboard/sidebar.blade.php
@@ -53,7 +53,7 @@
     </li>
 </ul>
 @endcan
-@can('pembina')
+@can('manajer')
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <div class="sidebar-brand-icon d-flex align-items-center justify-content-center">
         <img src="{{asset('assets/img/Logobgiputih.png')}}" alt="Logo" width="" height="80"></img>
@@ -65,21 +65,33 @@
     <hr class="sidebar-divider my-0">
     <!-- Nav Item -->
     <li class="nav-item">
-        <a class="nav-link" href="">
+        <a class="nav-link" href="{{route('manajer.index')}}">
             <i class="fa fa-light fa-user-pen"></i>
-            <span>Biodata</span>
+            <span>Beranda</span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="">
-            <i class="fa fa-light fa-newspaper"></i>
-            <span>Lowongan</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="">
+        <a class="nav-link" href="{{route('manajer.divisi')}}">
             <i class="fa fa-info-circle"></i>
-            <span>Status Lamaran</span>
+            <span>Data Divisi</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('manajer.jeniscuti')}}">
+            <i class="fa fa-info-circle"></i>
+            <span>Data Jenis Cuti</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('manajer.cuti')}}">
+            <i class="fa fa-info-circle"></i>
+            <span>Cuti</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('manajer.karyawan')}}">
+            <i class="fa fa-light fa-newspaper"></i>
+            <span>Data Karyawan</span>
         </a>
     </li>
     <!-- Divider -->
