239e5f74709664f618c40086a9a511688898cae8
soden46 <syarifsoden22@gmail.com>
2024-06-16 13:00:03 +0700

    Update

--- app/Http/Controllers/Manajer/ManajerController.php
+++ app/Http/Controllers/Manajer/ManajerController.php
@@ -25,21 +25,23 @@ class ManajerController extends Controller
         $manajer = Employee::where('npp', auth()->user()->npp)->first();
         // dd(auth()->user());
         $karyawan = Employee::where('hak_akses', 'karyawan')->where('hak_akses', 'karyawan')->where('id_divisi', $manajer->id_divisi)->where('active', 'Ya')->count();
-        $pending = CutiModel::with('jenisCuti', 'employee')
-            ->join('employee', 'cuti.npp', '=', 'employee.npp')
+        $pending = CutiModel::join('employee', 'cuti.npp', '=', 'employee.npp')
             ->where('employee.id_divisi', auth()->user()->id_divisi)
             ->where('stt_cuti', '=',  'Pending')
-            ->count();
-        $approve = CutiModel::with('jenisCuti', 'employee')
-            ->join('employee', 'cuti.npp', '=', 'employee.npp')
+            ->distinct('cuti.id')
+            ->count('cuti.id');
+
+        $approve = CutiModel::join('employee', 'cuti.npp', '=', 'employee.npp')
             ->where('employee.id_divisi', auth()->user()->id_divisi)
             ->where('stt_cuti', '=', 'Approved')
-            ->count();
-        $reject = CutiModel::with('jenisCuti', 'employee')
-            ->join('employee', 'cuti.npp', '=', 'employee.npp')
+            ->distinct('cuti.id')
+            ->count('cuti.id');
+
+        $reject = CutiModel::join('employee', 'cuti.npp', '=', 'employee.npp')
             ->where('employee.id_divisi', auth()->user()->id_divisi)
             ->where('stt_cuti', '=', 'Reject')
-            ->count();
+            ->distinct('cuti.id')
+            ->count('cuti.id');
         return view('manajer.index', compact('user', 'karyawan', 'pending', 'approve', 'reject'));
     }
 }
