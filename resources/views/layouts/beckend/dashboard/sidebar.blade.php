dd4e03e68b5ab4993d18d5de9b7f32b74b7cc8f8
Soden46 <syarifsoden22@gmail.com>
2024-06-18 23:09:58 +0700

    Update

--- resources/views/layouts/beckend/dashboard/sidebar.blade.php
+++ resources/views/layouts/beckend/dashboard/sidebar.blade.php
@@ -7,12 +7,13 @@
         </div>
         <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
             <div class="sidebar-brand-text mx-3">{{ Auth::user()->nama_emp }}</div>
-        </a>
-        <div class="sidebar-brand-brand mx-3 mt-0 " height="30">
-                    <p class="sidebar-brand-text align-items-center justify-content-center text-center text-white">Akun HRD</br>
+            <p class="sidebar-brand-text align-items-center justify-content-center text-center text-white">Akun HRD</br>
                     Sistem Informasi pengajuan Cuti Online</br>
                     <b>PT Gastro Gizi Sarana</b></br>
                     {{ \Carbon\Carbon::now()->translatedFormat('l, j F Y') }}</p>
+        </a>
+        <div class="sidebar-brand-brand mx-3 mt-0 " height="30">
+                    
         </div>
 
         <!-- Divider -->
